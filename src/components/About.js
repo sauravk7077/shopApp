3eb1bc08c3b3d1292e4a32a5ab6afc03906e9010
sauravk7077 <sauravk7077@gmail.com>
2020-10-05 10:57:49 +0530

    added github icon

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function About() {
     return (
@@ -8,7 +9,10 @@ function About() {
             <Fade>
                 <h2>About</h2>
                 <p>This is a project by Saurav Kumar. To check out the source code of this project, click the button below.</p>
-                <div><a className="btn" href="https://github.com/sauravk7077/shopapp">Visit GitHub Page</a></div>
+                <div><a className="btn" href="https://github.com/sauravk7077/shopapp">
+                    <FontAwesomeIcon icon={faGithub}/>
+                    &nbsp;Visit GitHub Page
+                </a></div>
             </Fade>
         </section>
     )
