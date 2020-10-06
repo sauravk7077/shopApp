a3bdb9cb0fb9d13d59739ba5fc8e6a929c869aaf
sauravk7077 <sauravk7077@gmail.com>
2020-10-06 11:13:40 +0530

    build it and published it on page branch

---
+++ precache-manifest.194691f9ae833661ddc7aa7f2ae1f3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cc640b555771b69e6a9c8aea5df7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8d980a21d119076eb3",
+    "url": "/static/css/main.896615c3.chunk.css"
+  },
+  {
+    "revision": "06c1f3fcf85bd85d0485",
+    "url": "/static/js/2.36b09805.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/static/js/2.36b09805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8d980a21d119076eb3",
+    "url": "/static/js/main.8c77138b.chunk.js"
+  },
+  {
+    "revision": "88a167bceee3e0d11966",
+    "url": "/static/js/runtime-main.0572d465.js"
+  }
+]);
