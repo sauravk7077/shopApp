bd530c30369af9ad5d73c85cf7a667dc0fd2887e
sauravk7077 <sauravk7077@gmail.com>
2020-10-06 12:02:58 +0530

    minor change

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,10 @@ function Home() {
             <div className="items-container">
                 <Item id="dfl4" name="Koda Camera" price={500} mrp={800} image="cam.jpg"/>
                 <Item id="dfl3" name="Tomato" price={400} mrp={700} image="tomato.jpg"/>
-                <Item id="dfl8" name="Yellow Potato" price={400} mrp={700} image="Potato.jpg"/>
+                <Item id="dfl8" name="Yellow Potato" price={400} mrp={700} image="potato.jpg"/>
                 <Item id="dfl5" name="Bluetooth Speaker" price={400} mrp={700} image="bSpeaker.jpg"/>
                 <Item id="dfl3" name="Tomato" price={400} mrp={700} image="tomato.jpg"/>
-                <Item id="dgl8" name="Yellow Potato" price={400} mrp={700} image="Potato.jpg"/>
+                <Item id="dgl8" name="Yellow Potato" price={400} mrp={700} image="potato.jpg"/>
                 <Item id="dil8" name="Bluetooth Speaker" price={400} mrp={700} image="bSpeaker.jpg"/>
                 <Item id="drl7" name="Koda Camera" price={500} mrp={800} image="cam.jpg"/>
                 
